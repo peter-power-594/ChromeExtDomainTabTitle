685239fb04db037d44185ddecb7ce7be57ab4b85
Pierre-Henri Lavigne <contact@peter202202.ninja>
2023-11-21 00:14:35 +0900

    Hotfix: Releasing RC 1.0.0

--- dtt.js
+++ dtt.js
@@ -1,29 +1,45 @@
 (function( _w, _d ) {
 
-    var _t
-      , _h = ( _w.location.host || '' ).split( '.' );
-    if ( _h.length > 1 && /com/.test( _h[ _h.length - 1 ] ) ) {
-        _h.pop();
-    }
-    if ( _h[ 0 ] === 'www' ) {
-        _h.shift();
-    }
-    _t = _h.join( ' ' ).replace( /(?:^|\s)\S/g, function( a ) {
-        return a.toUpperCase();
-    });
-    _d.title = _t;
+	var _t,
+		_h = ( _w.location.host || '' ).split( '.' );
+	// Check Domain Name Extension Suffix
+	if ( _h.length > 1 && /com/.test( _h[ _h.length - 1 ] ) ) {
+		_h.pop();
+	}
+	else {
+		if ( _h[ _h.length - 1 ] && _h[ _h.length - 1 ].length < 5 ) {
+			_h[ _h.length - 1 ] = _h[ _h.length - 1 ].toUpperCase();
+		}
+		if ( _h[ _h.length - 2 ] && _h[ _h.length - 2 ].length < 5 ) {
+			_h[ _h.length - 2 ] = _h[ _h.length - 2 ].toUpperCase();
+		}
+	}
+	// Check Subdomain Prefix
+	if ( _h[ 0 ] === 'www' ) {
+		_h.shift();
+	}
+	// Capitalize Keywords by Default
+	for ( let i = 0; i < _h.length; i++ ) {
+		if ( _h[ i ].length > 4 ) {
+			_h[ i ] = _h[ i ].toLowerCase().replace( /(^\S|[-|\s]\S)/g, function( a ) {
+				return a.replace( /[^\w]/, '' ).toUpperCase();
+			});
+		}
+	}
+	_t = _h.join( ' ' ); 
+	_d.title = _t;
 
-    new MutationObserver(function() {
-        if ( _d.title != _t ) {
-            _d.title = _t;
-        }
-    }).observe(
-        document.body
-      , {
-            attributes:    false
-          , childList:     true
-          , characterData: false
-        }
-    );
+	new MutationObserver(function() {
+		if ( _d.title !== _t ) {
+			_d.title = _t;
+		}
+	}).observe(
+		_d.body, {
+			attributes:	false,
+			childList: true,
+			subtree: true,
+			characterData: false
+		}
+	);
 
 })( window, document );
