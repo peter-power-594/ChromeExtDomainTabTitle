f77a8e7c6e9639cd90d318446be53aaf98381549
Pierre-Henri Lavigne <lavigne.pierrehenri@proton.me>
2025-03-16 22:29:17 +0900

    Hotfix: Ready for RC 1.1.0

--- dtt.js
+++ dtt.js
@@ -1,17 +1,18 @@
 (function( _w, _d ) {
 
-	var _t,
-		_h = ( _w.location.host || '' ).split( '.' );
+	let _h = ( _w.location ? _w.location.host || '' : '' ).split( '.' );
+	if ( ! _h.length ) {
+		return;
+	}
 	// Check Domain Name Extension Suffix
 	if ( _h.length > 1 && /com/.test( _h[ _h.length - 1 ] ) ) {
 		_h.pop();
 	}
 	else {
-		if ( _h[ _h.length - 1 ] && _h[ _h.length - 1 ].length < 5 ) {
-			_h[ _h.length - 1 ] = _h[ _h.length - 1 ].toUpperCase();
-		}
-		if ( _h[ _h.length - 2 ] && _h[ _h.length - 2 ].length < 5 ) {
-			_h[ _h.length - 2 ] = _h[ _h.length - 2 ].toUpperCase();
+		for ( let i = 1; i < 3; i++ ) {
+			if ( _h[ _h.length - i ] && _h[ _h.length - i ].length < 5 ) {
+				_h[ _h.length - i ] = _h[ _h.length - i ].toUpperCase();
+			}			
 		}
 	}
 	// Check Subdomain Prefix
@@ -20,18 +21,21 @@
 	}
 	// Capitalize Keywords by Default
 	for ( let i = 0; i < _h.length; i++ ) {
-		if ( _h[ i ].length > 4 ) {
+		if ( _h[ i ].length > 3 ) {
 			_h[ i ] = _h[ i ].toLowerCase().replace( /(^\S|[-|\s]\S)/g, function( a ) {
 				return a.replace( /[^\w]/, '' ).toUpperCase();
 			});
 		}
 	}
-	_t = _h.join( ' ' ); 
-	_d.title = _t;
+	var _t = _h.join( ' ' ),
+		_s = ''; 
+	_d.title = _t + ' | ' + _d.title;
+	_s = _d.title; 
 
 	new MutationObserver(function() {
-		if ( _d.title !== _t ) {
-			_d.title = _t;
+		if ( _d.title !== _s ) {
+			_s = _t + ' | ' + _d.title;
+			_d.title = _s;
 		}
 	}).observe(
 		_d.body, {
